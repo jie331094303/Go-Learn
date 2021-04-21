7448e1c089b6b213c7098964d7aa16f3d954a134
linshengjie <331094303@qq.com>
2021-04-22 00:24:40 +0800

    learn summary

--- Day1/main.go
+++ Day1/main.go
@@ -4,6 +4,23 @@ package main
 
 import "fmt"
 
+func main() {
+	test()
+}
+
+func test() {
+	//addTest()
+	f1Test()
+}
+
+func addTest() {
+	tmp1 := add(10)               //10没有被销毁
+	fmt.Println(tmp1(1), tmp1(2)) //11 13
+	// 此时tmp1和tmp2不是一个实体了
+	tmp2 := add(100)
+	fmt.Println(tmp2(1), tmp2(2))
+}
+
 func add(base int) func(int) int {
 	return func(i int) int {
 		base += i
@@ -11,10 +28,14 @@ func add(base int) func(int) int {
 	}
 }
 
-func main() {
-	tmp1 := add(10)               //10没有被销毁
-	fmt.Println(tmp1(1), tmp1(2)) //11 13
-	// 此时tmp1和tmp2不是一个实体了
-	tmp2 := add(100)
-	fmt.Println(tmp2(1), tmp2(2))
+func f1Test() {
+	fun := f1(100)
+	result := fun(11)
+	fmt.Println(result)
+}
+
+func f1(num int) (returnA func(rAParam int) int) {
+	return func(b int) int {
+		return num + b
+	}
 }
